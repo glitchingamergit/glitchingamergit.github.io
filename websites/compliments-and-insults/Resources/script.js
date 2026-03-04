99b8e531fa130f06713b0b0bcad976b5f9061409
glitching234 <ishaan9723@gmail.com>
2026-03-04 15:21:17 -0800

    New Website; it is Compliments & Insults

---
+++ websites/compliments-and-insults/Resources/script.js
@@ -0,0 +1,69 @@
+let outputText;
+let insults = [];
+let compliments = [];
+
+async function loadJson(path) {
+    try {
+        const response = await fetch(path);
+        if (!response.ok) throw Error(response.status);
+        return await response.json();
+    } catch (error) {
+        console.error("Failed to load external sites:", error);
+        return [];
+    }
+}
+
+window.addEventListener("DOMContentLoaded", async () => {
+    outputText = document.getElementById("output");
+    if (outputText) {
+        outputText.style.display = "none";
+    }
+
+    compliments = await loadJson('compliments.json');
+    insults = await loadJson('insults.json');
+
+    const args = window.location.search;
+    const searchParams = new URLSearchParams(args);
+
+    if (searchParams.get('message') !== null) {
+        if (outputText !== null) {
+            outputText.style.display = "block";
+            outputText.textContent = searchParams.get('message');
+        }
+    } else if (searchParams.get('m') !== null) {
+        if (outputText !== null) {
+            outputText.style.display = "block";
+            outputText.textContent = searchParams.get('m');
+        }
+    }
+});
+
+async function ShowCompliment() {
+    if (compliments.length <= 0) {
+        compliments = await loadJson('compliments.json');
+    }
+
+    if (compliments.length > 0) {
+        const randomIndex = Math.floor(Math.random() * compliments.length);
+        const compliment = compliments[randomIndex];
+
+        if (compliment === outputText.textContent) ShowCompliment();
+        outputText.textContent = compliment.toString();
+        outputText.style.display = "block";
+    }
+}
+
+async function ShowInsult() {
+    if (insults.length <= 0) {
+        insults = await loadJson('insults.json');
+    }
+
+    if (insults.length > 0) {
+        const randomIndex = Math.floor(Math.random() * insults.length);
+        const insult = insults[randomIndex];
+
+        if (insult === outputText.textContent) ShowInsult();
+        outputText.textContent = insult.toString();
+        outputText.style.display = "block";
+    }
+}
