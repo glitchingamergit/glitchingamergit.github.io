e2f730037ab8afb7dd5734159288e64f37190c20
glitching234 <ishaan9723@gmail.com>
2026-03-08 12:57:57 -0700

    Weekday work 2

--- about-me/scripts/script.js
+++ about-me/scripts/script.js
@@ -1,18 +1,18 @@
 const API_KEY = 'AIzaSyClWEXfT8kO6MvYdUClo602_UxysSWuuUA';
 const CHANNEL_ID = 'UCDDupcLC-TB0g34qXX6Pexw';
 
-const apiUrl = `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&channelId=${CHANNEL_ID}&part=snippet,id&order=date&maxResults=1`;
+const apiUrl = `https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&channelId=${CHANNEL_ID}&part=snippet,id&order=date&maxResults=1&type=video`;
 
 fetch(apiUrl)
     .then(response => response.json())
     .then(data => {
         if (data.items && data.items.length > 0) {
             const latestVideoId = data.items[0].id.videoId;
+            
             const iframe = document.getElementById('latestVideoEmbed');
-
             iframe.src = `https://www.youtube-nocookie.com/embed/${latestVideoId}`;
         } else {
-            console.log("No videos found.");
+            console.log("No public videos found on this channel.");
         }
     })
-    .catch(error => console.error('Error fetching latest video:', error));
+    .catch(error => console.error('Oops! Error fetching the latest video:', error));
