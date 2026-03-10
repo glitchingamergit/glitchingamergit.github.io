077637a5f21b1c906121d4716e1fa12e495ea35a
glitching234 <ishaan9723@gmail.com>
2026-03-09 17:45:41 -0700

    Added days until day/NEW WEBSITEclearclear

---
+++ websites/days-until-day/Resources/script.js
@@ -0,0 +1,59 @@
+function getCountdown(month, day, year, message) {
+    month = parseInt(month, 10) - 1;
+    day = parseInt(day, 10);
+
+    if (!message) {
+        message = "Unknown Day";
+    }
+
+    const now = new Date();
+    now.setHours(0, 0, 0, 0);
+    
+    if (year === null || year <= now.getFullYear()) {
+        year = now.getFullYear();
+    }
+
+    let dateOfDay = new Date(year, month, day);
+
+    if (now > dateOfDay) {
+        dateOfDay = new Date(year + 1, month, day);
+    }
+
+    const diff = dateOfDay - now;
+    const days = Math.round(diff / (1000 * 60 * 60 * 24));
+
+    let daysOrDay = "days";
+    let isOrAre = "are";
+
+    if (days === 1) {
+        daysOrDay = "day";
+        isOrAre = "is";
+    }
+
+    document.getElementById("output").textContent = "Output: There " + isOrAre + " " + days + " " + daysOrDay + " until " + message + "!";
+    return days;
+}
+
+getCountdown(4, 15, null, "Tax Day");
+
+let requestedMonth = document.getElementById("mm");
+let requestedDay = document.getElementById("dd");
+let requestedYear = document.getElementById("yy");
+let requestedName = document.getElementById("label");
+
+function ClearAll() {
+    requestedMonth.value = "";
+    requestedDay.value = "";
+    requestedYear.value = "";
+    requestedName.value = "";
+}
+
+function SubmitRequest() {
+    if (!requestedMonth.value || !requestedDay.value) {
+        ClearAll();
+        alert("Please put in the number of the month as well as the day.");
+        return;
+    }
+
+    getCountdown(requestedMonth.value, requestedDay.value, requestedYear.value, requestedName.value);
+}
