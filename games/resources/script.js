d926d62dd72510c19e69d77ca56a062756f23bb5
glitching234 <ishaan9723@gmail.com>
2026-08-16 10:46:56 -0700

    fixes that should be added months ago

--- games/resources/script.js
+++ games/resources/script.js
@@ -31,40 +31,50 @@ const game = params.get('game')?.toLowerCase();
 if (game !== "undefined") {
     switch (game) {
         case 'counter':
-            OpenGame('websites/games/ownProjects/Counter/index.html', 1080, 1920, true);
+            OpenGame('/games/ownProjects/Counter/index.html', 1080, 1920, true);
             break;
+        case 'dino':
+            OpenGame('/games/ownProjects/dino/index.html', 1280, 720, true);
         case 'flappybird':
-            OpenGame('websites/games/ownProjects/flappy/index.html', 1280, 720, true);
+            OpenGame('/games/ownProjects/flappy/index.html', 1280, 720, true);
             break;
         case 'fnae':
-            OpenGame('/websites/games/featured/fnae.html', 1280, 720, true);
+            OpenGame('/games/featured/fnae.html', 1280, 720, true);
             break;
         case 'fnaf':
-            OpenGame('/websites/games/featured/fnaf.html', 1280, 720, true);
+            OpenGame('/games/featured/fnaf.html', 1280, 720, true);
             break;
         case 'granny':
-            OpenGame('/websites/games/featured/granny.html', 1280, 720, true);
+            OpenGame('/games/featured/granny.html', 1280, 720, true);
             break;
         case 'learntofly':
-            OpenGame('/websites/games/featured/learn_to_fly.html', 1280, 720, true);
+            OpenGame('/games/featured/learn_to_fly.html', 1280, 720, true);
             break;
         case 'run':
-            OpenGame('/websites/games/featured/run_iii.html', 1116, 839, true);
+            OpenGame('/games/featured/run_iii.html', 1116, 839, true);
             break;
         case 'slope':
-            OpenGame('/websites/games/featured/slope.html', 1280, 720, true);
+            OpenGame('/games/featured/slope.html', 1280, 720, true);
             break;
         case 'tag':
-            OpenGame('/websites/games/featured/tag.html', 1280, 720, true);
+            OpenGame('/games/featured/tag.html', 1280, 720, true);
             break;
         case 'thatsnotmyneighbor':
-            OpenGame('/websites/games/featured/thats_not_my_neighbor.html', 1280, 720, true);
+            OpenGame('/games/featured/thats_not_my_neighbor.html', 1280, 720, true);
             break;
     }
 }
 
 function DownloadGame(winLink, macLink) {
-    if (navigator.userAgent.toLowerCase().includes('mac') || navigator.userAgent.toLowerCase().includes('linux')) {
+    if (navigator.userAgent.toLowerCase().includes('mac')) {
+        const link = document.createElement('a');
+        link.href = macLink;
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    } else if (navigator.userAgent.toLowerCase().includes('linux')) {
+        alert("You are using Linux, I didn't test the game on the operating system so it may not work. -glitchingamergit");
+
         const link = document.createElement('a');
         link.href = macLink;
         document.body.appendChild(link);
